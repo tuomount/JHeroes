724786740c163b1ce259d0df9b33ddfab938e27d
Tuomo Untinen <tuomo.untinen@iki.fi>
2016-03-13 09:24:30 +0200

    Added more races and experience modification calculation.

--- src/org/jheroes/map/character/CharacterRace.java
+++ src/org/jheroes/map/character/CharacterRace.java
@@ -34,13 +34,29 @@ public enum CharacterRace {
       + "mind affecting but have weakness against electricity.",false,true),
   RAT(4,"Rat","Rats are immune to poison but have weakness against fire.",false,false),
   LIVESTOCK(5,"Livestock","Livestock is animal that can be herded. Livestock has weakness"
-      + "against non lethat and fire attacks.",false,false),
+      + "against non lethal and fire attacks.",false,false),
   WATERELEMENTAL(6,"Waterelemental","Waterelementals are magical creatures "
       + "consisting of water. They are immune to fire, poison attacks but have weakness"
       + "against electric and ice attacks.",false,false),
-  INSECT(7,"Insect","Insects are animals with three bodies and six legs. They "
-      + "weakness against blunt and fire attacks. Insects are immune to mind "
-      + "affecting attacks.",false,false);
+  INSECT(7,"Insect","Insects are animals with three bodies and six legs. They have "
+      + "weakness against blunt, fire attacks and ice attacks. Insects are immune to mind "
+      + "affecting attacks.",false,false),
+  SPIDER(8,"Spider","Spiders are animals with two bodies and eight legs. They have"
+      + "weakness against blunt, fire attacks and ice attacks. Insects are immune to mind "
+      + "affecting attacks and poison.",false,false),
+  CAT(9,"Cat","Cats are immune to mind-affecting but have weakness against ice.",false,false),
+  SNAKE(10,"Snake","Snakes are carnivours serpents. They have weakness against ice attacks and"
+      + "resistance against fire attacks. Snakes are immune to poison.",false,false),
+  HOBGOBLIN(11,"Hobgoblin","Hobgoblins are humanoid like creatures. Hobgoblins have weakness "
+      + "againts mind-affecting attacks. They do have good resistance against ice attacks.",false,false),
+  TROLL(12,"Troll","Trolls are big and strong humanoid like creatures. Trolls have weakness "
+      + "againts fire and magic attacks. They have resistance against non-lethal and ice attacks.",false,false),
+  SKELETON(13,"Skeleton","Skeletons are undead creatures which are immune to poison and "
+      + "mind affecting but have weakness against blunt-weapons.",false,true),
+  IMP(14,"Imp","Imp are flying evil creatures which are immune to fire and "
+      + "magic attacks but have weakness against electricty and mind-affecting attacks.",false,false),
+  BRAINHORROR(15,"Brain horror","Brain horrors are walking brain creatures which are immune to "
+      + "mind-affecting and magic attacks but have weakness against poison and normal attacks.",false,false);
   
   CharacterRace(int index,String name, String description, boolean isPlayerRace,
       boolean isUndead) {
@@ -50,7 +66,6 @@ public enum CharacterRace {
     this.name = name;
     this.description = description;
   }
-  
   
   
   /**
@@ -125,6 +140,7 @@ public enum CharacterRace {
    */
   public DamageModifier damageModifierForNormal() {
     switch (this) {
+    case BRAINHORROR: {return DamageModifier.WEAKNESS;}
     default:
       return DamageModifier.NORMAL;
     }
@@ -137,6 +153,8 @@ public enum CharacterRace {
   public DamageModifier damageModifierForBlunt() {
     switch (this) {
     case INSECT: {return DamageModifier.WEAKNESS;}
+    case SPIDER: {return DamageModifier.WEAKNESS;}
+    case SKELETON: {return DamageModifier.WEAKNESS;}
     default:
       return DamageModifier.NORMAL;
     }
@@ -153,6 +171,7 @@ public enum CharacterRace {
     }
     switch (this) {
     case LIVESTOCK: {return DamageModifier.WEAKNESS;}
+    case TROLL: {return DamageModifier.RESISTANCE;}
     default:
       return DamageModifier.NORMAL;
     }
@@ -166,6 +185,7 @@ public enum CharacterRace {
     switch (this) {
     case ZOMBIE: {return DamageModifier.WEAKNESS;}
     case WATERELEMENTAL: {return DamageModifier.WEAKNESS;}
+    case IMP: {return DamageModifier.WEAKNESS;}
     default:
       return DamageModifier.NORMAL;
     }
@@ -179,6 +199,12 @@ public enum CharacterRace {
     switch (this) {
     case ELF: {return DamageModifier.WEAKNESS;}
     case WATERELEMENTAL: {return DamageModifier.WEAKNESS;}
+    case INSECT: {return DamageModifier.WEAKNESS;}
+    case SPIDER: {return DamageModifier.WEAKNESS;}
+    case CAT: {return DamageModifier.WEAKNESS;}
+    case SNAKE: {return DamageModifier.WEAKNESS;}
+    case HOBGOBLIN: {return DamageModifier.RESISTANCE;}
+    case TROLL: {return DamageModifier.RESISTANCE;}
     default:
       return DamageModifier.NORMAL;
     }
@@ -194,6 +220,10 @@ public enum CharacterRace {
     case LIVESTOCK: {return DamageModifier.WEAKNESS;}
     case WATERELEMENTAL: {return DamageModifier.IMMUNITY;}
     case INSECT: {return DamageModifier.WEAKNESS;}
+    case SPIDER: {return DamageModifier.WEAKNESS;}
+    case SNAKE: {return DamageModifier.RESISTANCE;}
+    case TROLL: {return DamageModifier.WEAKNESS;}
+    case IMP: {return DamageModifier.IMMUNITY;}
     default:
       return DamageModifier.NORMAL;
     }
@@ -208,6 +238,10 @@ public enum CharacterRace {
     case ZOMBIE: {return DamageModifier.IMMUNITY;}
     case RAT: {return DamageModifier.RESISTANCE;}
     case WATERELEMENTAL: {return DamageModifier.IMMUNITY;}
+    case SPIDER: {return DamageModifier.IMMUNITY;}
+    case SNAKE: {return DamageModifier.IMMUNITY;}
+    case SKELETON: {return DamageModifier.IMMUNITY;}
+    case BRAINHORROR: {return DamageModifier.WEAKNESS;}
     default:
       return DamageModifier.NORMAL;
     }
@@ -221,6 +255,12 @@ public enum CharacterRace {
     switch (this) {
     case ZOMBIE: {return DamageModifier.IMMUNITY;}
     case INSECT: {return DamageModifier.IMMUNITY;}
+    case SPIDER: {return DamageModifier.IMMUNITY;}
+    case CAT: {return DamageModifier.IMMUNITY;}
+    case HOBGOBLIN: {return DamageModifier.WEAKNESS;}
+    case SKELETON: {return DamageModifier.IMMUNITY;}
+    case IMP: {return DamageModifier.WEAKNESS;}
+    case BRAINHORROR: {return DamageModifier.IMMUNITY;}
     default:
       return DamageModifier.NORMAL;
     }
@@ -232,9 +272,108 @@ public enum CharacterRace {
    */
   public DamageModifier damageModifierForMagic() {
     switch (this) {
+    case TROLL: {return DamageModifier.WEAKNESS;}
+    case IMP: {return DamageModifier.IMMUNITY;}
+    case BRAINHORROR: {return DamageModifier.IMMUNITY;}
     default:
       return DamageModifier.NORMAL;
     }
+  }
+
+  /**
+   * Returns experience modifier. Value is between 50-300. Experience should
+   * be first multiplied with this and then divide with 100. So return value
+   * is actually percentage modifier for monster experience
+   * @return int 50-300
+   */
+  public int getExpModifier() {
+    int result = 100;
+    // Blunt weapons about half weapons are blunt thus bigger effect
+    DamageModifier mod = this.damageModifierForBlunt();
+    switch (mod) {
+    case WEAKNESS: {result = result -12;break;}
+    case RESISTANCE: {result = result +12;break;}
+    case IMMUNITY: {result = result +24;break;}
+    default:
+      break;
+    }
+    // Other half of weapons are normal, and quite many spells too
+    mod = this.damageModifierForNormal();
+    switch (mod) {
+    case WEAKNESS: {result = result -15;break;}
+    case RESISTANCE: {result = result +15;break;}
+    case IMMUNITY: {result = result +30;break;}
+    default:
+      break;
+    }
+    // Non lethal is basicly hand and stamina damage from actions.
+    mod = this.damageModifierForNonLethal();
+    switch (mod) {
+    case WEAKNESS: {result = result -8;break;}
+    case RESISTANCE: {result = result +8;break;}
+    case IMMUNITY: {result = result +16;break;}
+    default:
+      break;
+    }
+    mod = this.damageModifierForFire();
+    switch (mod) {
+    case WEAKNESS: {result = result -10;break;}
+    case RESISTANCE: {result = result +10;break;}
+    case IMMUNITY: {result = result +20;break;}
+    default:
+      break;
+    }
+    mod = this.damageModifierForIce();
+    switch (mod) {
+    case WEAKNESS: {result = result -10;break;}
+    case RESISTANCE: {result = result +10;break;}
+    case IMMUNITY: {result = result +20;break;}
+    default:
+      break;
+    }
+    mod = this.damageModifierForElectricity();
+    switch (mod) {
+    case WEAKNESS: {result = result -10;break;}
+    case RESISTANCE: {result = result +10;break;}
+    case IMMUNITY: {result = result +20;break;}
+    default:
+      break;
+    }
+    mod = this.damageModifierForMagic();
+    switch (mod) {
+    case WEAKNESS: {result = result -12;break;}
+    case RESISTANCE: {result = result +12;break;}
+    case IMMUNITY: {result = result +24;break;}
+    default:
+      break;
+    }
+    mod = this.damageModifierForMindAffecting();
+    // Mind effecting has different change to hit than others thus making
+    // it a bit more "valuable"
+    switch (mod) {
+    case WEAKNESS: {result = result -12;break;}
+    case RESISTANCE: {result = result +12;break;}
+    case IMMUNITY: {result = result +24;break;}
+    default:
+      break;
+    }
+    // There are only few ways to give poisoning to monster thus value it
+    // lower.
+    mod = this.damageModifierForPoison();
+    switch (mod) {
+    case WEAKNESS: {result = result -8;break;}
+    case RESISTANCE: {result = result +8;break;}
+    case IMMUNITY: {result = result +16;break;}
+    default:
+      break;
+    }
+    if (result < 50) {
+      result = 50;
+    }
+    if (result > 300) {
+      result = 300;
+    }
+    return result;
   }
 
 }
